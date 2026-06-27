0d11430a2dca23398322070d1008e481d0a4f651
Vi Ramen <vi.is.chapmann@gmail.com>
2026-06-27 22:19:12 +0300

    Initial commit

--- src/log.rs
+++ src/log.rs
@@ -38,21 +38,21 @@ pub macro trace($($arg:tt)+) {
 }
 
 pub macro debug($($arg:tt)+) {
-    crate::sym::k_log(AttLvl::Trace, concat!(env!("CARGO_PKG_NAME"), "::", module_path!()), file!(), line!(), format_args!($($arg)+));
+    crate::sym::k_log(AttLvl::Debug, concat!(env!("CARGO_PKG_NAME"), "::", module_path!()), file!(), line!(), format_args!($($arg)+));
 }
 
 pub macro info($($arg:tt)+) {
-    crate::sym::k_log(AttLvl::Trace, concat!(env!("CARGO_PKG_NAME"), "::", module_path!()), file!(), line!(), format_args!($($arg)+));
+    crate::sym::k_log(AttLvl::Info, concat!(env!("CARGO_PKG_NAME"), "::", module_path!()), file!(), line!(), format_args!($($arg)+));
 }
 
 pub macro warn($($arg:tt)+) {
-    crate::sym::k_log(AttLvl::Trace, concat!(env!("CARGO_PKG_NAME"), "::", module_path!()), file!(), line!(), format_args!($($arg)+));
+    crate::sym::k_log(AttLvl::Warn, concat!(env!("CARGO_PKG_NAME"), "::", module_path!()), file!(), line!(), format_args!($($arg)+));
 }
 
 pub macro error($($arg:tt)+) {
-    crate::sym::k_log(AttLvl::Trace, concat!(env!("CARGO_PKG_NAME"), "::", module_path!()), file!(), line!(), format_args!($($arg)+));
+    crate::sym::k_log(AttLvl::Error, concat!(env!("CARGO_PKG_NAME"), "::", module_path!()), file!(), line!(), format_args!($($arg)+));
 }
 
 pub macro panic_msg($($arg:tt)+) {
-    crate::sym::k_log(AttLvl::Trace, concat!(env!("CARGO_PKG_NAME"), "::", module_path!()), file!(), line!(), format_args!($($arg)+));
+    crate::sym::k_log(AttLvl::Panic, concat!(env!("CARGO_PKG_NAME"), "::", module_path!()), file!(), line!(), format_args!($($arg)+));
 }
--- src/sym.rs
+++ src/sym.rs
@@ -1,12 +1,14 @@
-use crate::stub;
+use crate::{stub, log::AttLvl};
+use core::fmt::Arguments;
 
 crate::util::SYMBOL! {
-    pub k_panic : fn (&core::panic::PanicInfo) -> !     = stub::panic   ;
+    pub k_panic : fn (&core::panic::PanicInfo) -> !                         = stub::panic   ;
 
-    pub k_exit  : fn (i32) -> !                         = stub::exit    ;
+    pub k_exit  : fn (i32) -> !                                             = stub::exit    ;
 
-    pub k_alloc : fn (core::alloc::Layout) -> *mut u8   = stub::alloc   ;
+    pub k_alloc : fn (core::alloc::Layout) -> *mut u8                       = stub::alloc   ;
 
-    pub k_free  : fn (*mut u8, core::alloc::Layout)     = stub::free    ;
-    pub k_log   : fn (crate::log::AttLvl, &'static str, &'static str, u32, core::fmt::Arguments<'_>) = stub::log;
+    pub k_free  : fn (*mut u8, core::alloc::Layout)                         = stub::free    ;
+
+    pub k_log   : fn (AttLvl,&'static str,&'static str,u32,Arguments<'_>)   = stub::log     ;
 }
