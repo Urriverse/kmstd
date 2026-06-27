79c3b6480c232ff23c2d5886d941fde68dfdb1b4
Vi Ramen <vi.is.chapmann@gmail.com>
2026-06-27 20:14:50 +0300

    Initial commit

---
+++ src/ga.rs
@@ -0,0 +1,15 @@
+pub macro global_allocator() {
+    struct GA;
+
+    unsafe impl core::alloc::GlobalAlloc for GA {
+        unsafe fn alloc(&self, layout: core::alloc::Layout) -> *mut u8 {
+            crate::KeInvoke!(alloc: layout)
+        }
+
+        unsafe fn dealloc(&self, ptr: *mut u8, layout: core::alloc::Layout) {
+            crate::KeInvoke!(free: ptr, layout)
+        }
+    }
+
+    #[global_allocator] static ___GAI: GA = GA;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,18 @@
 #![feature(decl_macro)]
 
 pub mod systab;
+pub mod ga;
 
-pub macro meta() {
-    #[unsafe(no_mangle)]
-    pub static SYSTAB: $crate::systab::KeSysTabPtr = $crate::systab::KeSysTabPtr(core::ptr::null());
+#[unsafe(no_mangle)]
+pub static SYSTAB: systab::KeSysTabPtr = systab::KeSysTabPtr(core::ptr::null());
+
+pub macro KeInvoke($n:ident: $($arg:expr),*) {
+    (
+        unsafe {
+            SYSTAB.0.as_ref_unchecked()
+        }.$n
+    )
+    ( $($arg),* )
 }
+
+ga::global_allocator!{}
