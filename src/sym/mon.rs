b6ad391d37b5293083b22d171068e8268a2b2039
Vi Ramen <vi.is.chapmann@gmail.com>
2026-07-01 10:46:53 +0300

    Initial commit

--- src/sym/mon.rs
+++ src/sym/mon.rs
@@ -1,15 +1,15 @@
 use ketypes::*;
 
-// Import! {
-//     pub fn MonLog
-//     (
-//         level: KeAttLvl,
-//         module: KeStr,
-//         file: KeStr,
-//         line: u32,
-//         args: Arguments<'_>
-//     ) where kernel 0.0 {
-//         let _ = (level, module, file, line, args);
-//         /* nothing to do: we can't even log this error */
-//     }
-// }
+Import! {
+    pub fn MonLog
+    (
+        level: KeAttLvl,
+        module: KeStr,
+        file: KeStr,
+        line: u32,
+        args: Arguments<'_>
+    ) where kernel 0.0 {
+        let _ = (level, module, file, line, args);
+        /* nothing to do: we can't even log this error */
+    }
+}
