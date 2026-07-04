96b8fae354e3e14e6879c77b557f068fef588127
Vi Ramen <vi.is.chapmann@gmail.com>
2026-07-04 13:26:26 +0300

    Initial commit

--- src/log.rs
+++ src/log.rs
@@ -1,12 +1,12 @@
-pub macro trace($($arg:tt)+){crate::MonLog(ketypes::mon::lvl::KeAttLvl::Trace,concat
+pub macro trace($($arg:tt)+){crate::MonLog(crate::AttLvl::Trace,concat
 !(env!("CARGO_PKG_NAME"),"::",module_path!()),file!(),line!(),format_args!($($arg)+));}
-pub macro debug($($arg:tt)+){crate::MonLog(ketypes::mon::lvl::KeAttLvl::Debug,concat
+pub macro debug($($arg:tt)+){crate::MonLog(crate::AttLvl::Debug,concat
 !(env!("CARGO_PKG_NAME"),"::",module_path!()),file!(),line!(),format_args!($($arg)+));}
-pub macro info($($arg:tt)+){crate::MonLog(ketypes::mon::lvl::KeAttLvl::Info,concat
+pub macro info($($arg:tt)+){crate::MonLog(crate::AttLvl::Info,concat
 !(env!("CARGO_PKG_NAME"),"::",module_path!()),file!(),line!(),format_args!($($arg)+));}
-pub macro warn($($arg:tt)+){crate::MonLog(ketypes::mon::lvl::KeAttLvl::Warn,concat
+pub macro warn($($arg:tt)+){crate::MonLog(crate::AttLvl::Warn,concat
 !(env!("CARGO_PKG_NAME"),"::",module_path!()),file!(),line!(),format_args!($($arg)+));}
-pub macro error($($arg:tt)+){crate::MonLog(ketypes::mon::lvl::KeAttLvl::Error,concat
+pub macro error($($arg:tt)+){crate::MonLog(crate::AttLvl::Error,concat
 !(env!("CARGO_PKG_NAME"),"::",module_path!()),file!(),line!(),format_args!($($arg)+));}
-pub macro panic_msg($($arg:tt)+){crate::MonLog(ketypes::mon::lvl::KeAttLvl::Panic,concat
+pub macro panic_msg($($arg:tt)+){crate::MonLog(crate::AttLvl::Panic,concat
 !(env!("CARGO_PKG_NAME"),"::",module_path!()),file!(),line!(),format_args!($($arg)+));}
