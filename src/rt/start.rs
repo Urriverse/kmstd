1a0771d67f611b426332cf28c26f9923f1ad6ef6
Vi Ramen <vi.is.chapmann@gmail.com>
2026-07-12 17:56:54 +0300

    Initial commit

--- src/rt/start.rs
+++ src/rt/start.rs
@@ -1,6 +1,5 @@
 pub type EntryPoint<'a> = &'a (dyn Fn() -> i32 + Sync + core::panic::RefUnwindSafe);
 
-#[cfg(not(test))]
 pub fn lang_start_internal(main: EntryPoint, argc: isize, argv: *const *const u8) -> !
 {
     let _ = (argc, argv);
@@ -8,4 +7,25 @@ pub fn lang_start_internal(main: EntryPoint, argc: isize, argv: *const *const u8
     main();
 
     crate::raw::ExecExit(0);
+}
+
+unsafe extern "Rust"
+{
+    fn main() -> ();
+}
+
+fn entry_point() -> i32
+{
+    unsafe
+    {
+        main();
+    }
+    
+    0
+}
+
+#[unsafe(no_mangle)]
+pub fn _start() -> !
+{
+    lang_start_internal(&entry_point, 0, 0 as _)
 }
